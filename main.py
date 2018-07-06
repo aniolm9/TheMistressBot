06c6733f9393ab8bffe52d75957630e0b65dcfe0
Aniol Marti <aniol@aniolmarti.cat>
2018-07-06 16:55:10 +0200

    Tanco el fitxer i evito passar el \n

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def main():
     # Call the logs function to start logging.
     functions.logs()
     # Create the Updater and pass the bot token;
-    TOKEN=open("TOKEN", "r").readline()
+    f = open("TOKEN", "r")
+    TOKEN = f.readline().rstrip()
+    f.close()
     updater = Updater(token=TOKEN)
     dispatcher = updater.dispatcher
 
